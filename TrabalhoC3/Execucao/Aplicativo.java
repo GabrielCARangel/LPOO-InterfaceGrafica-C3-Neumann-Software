88473980918cb68358f3ad7107793f93b0952b76
Gabriel C. A. Rangel <ansesquis@gmail.com>
2021-11-28 17:32:25 -0300

    Aplicativo preliminar
    Co-Authored-By: Matheus Barbosa <80155638+Barbomat@users.noreply.github.com>

--- TrabalhoC3/Execucao/Aplicativo.java
+++ TrabalhoC3/Execucao/Aplicativo.java
@@ -4,6 +4,11 @@
  */
 package TrabalhoC3.Execucao;
 
+import java.util.ArrayList;
+import TrabalhoC3.Classes.*;
+import java.text.SimpleDateFormat;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Gabriel
@@ -35,11 +40,12 @@ public class Aplicativo extends javax.swing.JFrame {
         CPF = new javax.swing.JLabel();
         CPFTextField = new javax.swing.JTextField();
         BotaoPesquisar = new javax.swing.JButton();
-        CPFScrollPane = new javax.swing.JScrollPane();
-        ListCPF = new javax.swing.JList<>();
         BotaoCreditos = new javax.swing.JButton();
+        SPDados = new javax.swing.JScrollPane();
+        TableDados = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
 
         Titulo.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
         Titulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -48,13 +54,23 @@ public class Aplicativo extends javax.swing.JFrame {
         MetodoDePesquisa.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         MetodoDePesquisa.setText("Método de pesquisa");
 
-        MetodoDePesquisaBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Árvore ABB", "Árvore AVL", "Hashing" }));
+        MetodoDePesquisaBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] {"", "Árvore ABB", "Árvore AVL", "Lista Hash" }));
+        MetodoDePesquisaBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MetodoDePesquisaBoxActionPerformed(evt);
+            }
+        });
 
         Arquivo1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         Arquivo1.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         Arquivo1.setText("Arquivo");
 
-        ArquivoBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "500 Elementos Aleatórios", "500 Elementos Invertidos", "500 Elementos Ordenados", "1000 Elementos Aleatórios", "1000 Elementos Invertidos", "1000 Elementos Ordenados", "5000 Elementos Aleatórios", "5000 Elementos Invertidos", "5000 Elementos Ordenados", "10000 Elementos Aleatórios", "10000 Elementos Invertidos", "10000 Elementos Ordenados", "50000 Elementos Aleatórios", "50000 Elementos Invertidos", "50000 Elementos Ordenados" }));
+        ArquivoBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] {"", "500 Elementos Aleatórios", "500 Elementos Invertidos", "500 Elementos Ordenados", "1000 Elementos Aleatórios", "1000 Elementos Invertidos", "1000 Elementos Ordenados", "5000 Elementos Aleatórios", "5000 Elementos Invertidos", "5000 Elementos Ordenados", "10000 Elementos Aleatórios", "10000 Elementos Invertidos", "10000 Elementos Ordenados", "50000 Elementos Aleatórios", "50000 Elementos Invertidos", "50000 Elementos Ordenados" }));
+        ArquivoBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ArquivoBoxActionPerformed(evt);
+            }
+        });
 
         CPF.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         CPF.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
@@ -67,14 +83,33 @@ public class Aplicativo extends javax.swing.JFrame {
             }
         });
 
-        CPFScrollPane.setViewportView(ListCPF);
-
         BotaoCreditos.setText("Créditos");
         BotaoCreditos.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 BotaoCreditosMouseClicked(evt);
             }
         });
+
+        TableDados.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Nome", "CPF", "Data", "Valor"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.Long.class, java.lang.String.class, java.lang.Double.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        SPDados.setViewportView(TableDados);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -83,8 +118,8 @@ public class Aplicativo extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(SPDados)
                     .addComponent(Titulo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(CPFScrollPane, javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addGroup(jPanel1Layout.createSequentialGroup()
@@ -123,15 +158,12 @@ public class Aplicativo extends javax.swing.JFrame {
                     .addComponent(CPF, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(CPFTextField))
                 .addGap(18, 18, 18)
-                .addComponent(CPFScrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, 327, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(SPDados, javax.swing.GroupLayout.PREFERRED_SIZE, 368, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(BotaoCreditos)
-                        .addGap(6, 6, 6))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(BotaoPesquisar)
-                        .addContainerGap())))
+                    .addComponent(BotaoCreditos)
+                    .addComponent(BotaoPesquisar))
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -149,13 +181,276 @@ public class Aplicativo extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void BotaoCreditosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotaoCreditosMouseClicked
-        // TODO add your handling code here:
         new Creditos().setVisible(true);
     }//GEN-LAST:event_BotaoCreditosMouseClicked
 
     private void BotaoPesquisarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BotaoPesquisarMouseClicked
-        // TODO add your handling code here:
+
+        ArrayList<Compra> retornoCompras = new ArrayList<>();
+        String CPF = CPFTextField.getText();
+
+        DefaultTableModel tabela = (DefaultTableModel) TableDados.getModel();
+        tabela.getDataVector().removeAllElements();
+        tabela.fireTableDataChanged();
+
+        try {
+
+            if (CPFTextField.getText().length() != 0) {
+
+                retornoCompras = ExecutarPesquisas.pesquisarCPF(tipoPesquisa, quantidadeDados, CPF);
+                tabelaDados(retornoCompras);
+            
+            } else {
+
+                switch (tipoPesquisa) {
+                    
+                    case 1:
+                            retornoCompras = ExecutarPesquisas.pesquisaABB(quantidadeDados);
+                            tabelaDados(retornoCompras);
+                        break;
+
+                    case 2:
+                            retornoCompras = ExecutarPesquisas.pesquisaAVL(quantidadeDados);
+                            tabelaDados(retornoCompras);
+                        break;
+
+                    case 3: 
+                            retornoCompras = ExecutarPesquisas.pesquisaHash(quantidadeDados);
+                            tabelaDados(retornoCompras);  
+                        break;
+
+                    default: 
+                }
+            }
+
+        } catch (StackOverflowError erro) {
+            
+            System.out.print("\nErro na Árvore ABB de StackOverflow.\n");
+        }
     }//GEN-LAST:event_BotaoPesquisarMouseClicked
+
+    private void MetodoDePesquisaBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MetodoDePesquisaBoxActionPerformed
+ 
+        String escolha = (String) MetodoDePesquisaBox.getSelectedItem();
+
+        switch (escolha) {
+
+            case "Árvore ABB":
+                    System.out.print("\nOpção 01");
+                    tipoPesquisa = 1;
+                break;
+
+            case "Árvore AVL":
+                    System.out.print("\nOpção 02");
+                    tipoPesquisa = 2;
+                break;
+
+            case "Lista Hash":
+                    System.out.print("\nOpção 03");
+                    tipoPesquisa = 3;
+                break;
+
+            default:
+        }
+
+        //escolhaPesquisa();
+    }//GEN-LAST:event_MetodoDePesquisaBoxActionPerformed
+
+    private void ArquivoBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ArquivoBoxActionPerformed
+        
+        String escolha = (String) ArquivoBox.getSelectedItem();
+
+        switch (escolha) {
+
+            case "500 Elementos Aleatórios":
+                    System.out.print("\nOpção 01");
+                    quantidadeDados = 1;
+                break;
+
+            case "500 Elementos Invertidos":
+                    System.out.print("\nOpção 02");
+                    quantidadeDados = 2;
+                break;
+
+            case "500 Elementos Ordenados":
+                    System.out.print("\nOpção 03");
+                    quantidadeDados = 3;
+                break;
+            
+             case "1000 Elementos Aleatórios":
+                    System.out.print("\nOpção 04");
+                    quantidadeDados = 4;
+                break;
+
+            case "1000 Elementos Invertidos":
+                    System.out.print("\nOpção 05");
+                    quantidadeDados = 5;
+                break;
+
+            case "1000 Elementos Ordenados":
+                    System.out.print("\nOpção 06");
+                    quantidadeDados = 6;
+                break;
+            
+            case "5000 Elementos Aleatórios":
+                    System.out.print("\nOpção 07");
+                    quantidadeDados = 7;
+                break;
+
+            case "5000 Elementos Invertidos":
+                    System.out.print("\nOpção 08");
+                    quantidadeDados = 8;
+                break;
+
+            case "5000 Elementos Ordenados":
+                    System.out.print("\nOpção 09");
+                    quantidadeDados = 9;
+                break;
+            
+            case "10000 Elementos Aleatórios":
+                    System.out.print("\nOpção 10");
+                    quantidadeDados = 10;
+                break;
+
+            case "10000 Elementos Invertidos":
+                    System.out.print("\nOpção 11");
+                    quantidadeDados = 11;
+                break;
+
+            case "10000 Elementos Ordenados":
+                    System.out.print("\nOpção 12");
+                    quantidadeDados = 12;
+                break;
+                
+            case "50000 Elementos Aleatórios":
+                    System.out.print("\nOpção 13");
+                    quantidadeDados = 13;
+                break;
+
+            case "50000 Elementos Invertidos":
+                    System.out.print("\nOpção 14");
+                    quantidadeDados = 14;
+                break;
+
+            case "50000 Elementos Ordenados":
+                    System.out.print("\nOpção 15");
+                    quantidadeDados = 15;
+                break;
+            
+            default:
+        }
+
+        //escolhaPesquisa();
+    }//GEN-LAST:event_ArquivoBoxActionPerformed
+
+    /*private void adicionarTabela() {
+
+        String nome = "Gabriel", data = "01/01/2000";   
+        long cpf = 5;
+        double valor = 50;
+
+        DefaultTableModel test = (DefaultTableModel) TableDados.getModel();
+        test.setNumRows(0);
+        test.addRow(new Object[] {nome, cpf, data, valor});
+    }
+
+    private void escolhaPesquisa () {
+
+        ArrayList<Compra> retornoCompras = new ArrayList<>();
+
+        switch (tipoPesquisa) {
+
+            case 1://ABB
+                
+                
+                switch (quantidadeDados) {
+
+                    case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15:
+                            retornoCompras = ExecutarPesquisas.pesquisaABB(quantidadeDados);
+                            tabelaDados(retornoCompras);
+                        break;
+                    
+                    default:
+                }
+
+
+                switch (quantidadeDados) {
+                        
+                    case 1:
+                        
+                        adicionarTabela();
+                        System.out.print("\nMétodo Árvore ABB e Base 500 Ordenados");                     
+                        break;
+
+                    default:
+                }
+
+                break;
+
+            case 2://AVL
+                    switch (quantidadeDados) {
+
+                        case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15:
+                            retornoCompras = ExecutarPesquisas.pesquisaAVL(quantidadeDados);
+                            tabelaDados(retornoCompras);
+                            break;
+
+                        default:
+                    }
+                break;
+
+            case 3://Lista Hash
+                    switch (quantidadeDados) {
+
+                        case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15:
+                                retornoCompras = ExecutarPesquisas.pesquisaHash(quantidadeDados);
+                                tabelaDados(retornoCompras);
+                            break;
+                        
+                        default:
+                    }
+                break;
+            
+            default:
+        }
+    }*/
+
+
+    private void tabelaDados (ArrayList<Compra> retornoCompras) {
+
+        /*
+            DefaultTableModel test = (DefaultTableModel) TableDados.getModel();
+            test.setNumRows(0);
+            test.addRow(new Object[] {nome, cpf, data, valor});
+        */
+
+        //tabela.setNumRows(retornoCompras.size());
+            
+            String nome = "", data = "";   
+            long cpf = 0;
+            double valor = 0;
+            SimpleDateFormat dataSimples = new SimpleDateFormat("dd/MM/yyyy");
+            DefaultTableModel tabela = (DefaultTableModel) TableDados.getModel();
+            tabela.setNumRows(0);
+
+            //if (retornoCompras.size() > 0) {
+
+                for (int contador = 0; contador < retornoCompras.size(); contador++) {
+
+                    nome = retornoCompras.get(contador).getCliente().getNome();
+                    data = dataSimples.format(retornoCompras.get(contador).getData().getTime());
+                    cpf = retornoCompras.get(contador).getCliente().getCPF();
+                    valor = retornoCompras.get(contador).getValor();
+
+                    tabela.addRow(new Object[] {nome, cpf, data, valor});
+                }
+            
+            /*} else {
+
+                tabela.getDataVector().removeAllElements();
+                tabela.fireTableDataChanged();
+            }*/
+    }
 
     /**
      * @param args the command line arguments
@@ -168,7 +463,7 @@ public class Aplicativo extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
@@ -198,12 +493,14 @@ public class Aplicativo extends javax.swing.JFrame {
     private javax.swing.JButton BotaoCreditos;
     private javax.swing.JButton BotaoPesquisar;
     private javax.swing.JLabel CPF;
-    private javax.swing.JScrollPane CPFScrollPane;
     private javax.swing.JTextField CPFTextField;
-    private javax.swing.JList<String> ListCPF;
     private javax.swing.JLabel MetodoDePesquisa;
     private javax.swing.JComboBox<String> MetodoDePesquisaBox;
+    private javax.swing.JScrollPane SPDados;
+    private javax.swing.JTable TableDados;
     private javax.swing.JLabel Titulo;
     private javax.swing.JPanel jPanel1;
-    // End of variables declaration//GEN-END:variables
+    // End of variables declaration//GEN-END:variables    
+    private static byte tipoPesquisa;
+    private static byte quantidadeDados;
 }
